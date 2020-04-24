804190dae797e5a0905c9dd492129f65c8b602a4
rafaregisp <61069286+rafaregisp@users.noreply.github.com>
2020-04-24 15:32:10 -0300

    Create test_operacoes.py

---
+++ test_operacoes.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from com.rafael.operacoes Import Opercoes
+
+class testOperacoes (TestCase):
+
+   def setup(self):
+       self.operacoes = operacoes()
+       
+       def test_soma(self):
+           self.assertEqual(self.operacoes.soma([7, 2]), 9, "deveria ser 9")
