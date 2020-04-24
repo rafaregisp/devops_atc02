4cfa7c60eafac3eec4e349ba26751603a7baa5aa
rafaregisp <61069286+rafaregisp@users.noreply.github.com>
2020-04-24 14:42:55 -0300

    Create operacoes.py

---
+++ operacoes.py
@@ -0,0 +1,12 @@
+class operacoes():
+  def soma(self, valores):
+      val = 0
+      for v in valores:
+          val = val + v
+      return val
+   
+  def subtracao(self, valores):
+      val = 0
+      for v in valores:
+          val = val -v
+          return val
